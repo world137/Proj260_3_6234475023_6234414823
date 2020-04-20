772c1751840263969df8faecacf5b8f3bb8d3bae
world137 <world.sk137@gmail.com>
2020-04-20 17:28:43 +0700

    new layout

--- app/src/main/java/com/example/busolympic/SportEvent/MainActivity.java
+++ app/src/main/java/com/example/busolympic/SportEvent/MainActivity.java
@@ -14,6 +14,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     //เนื่องจากเพื่อนยังไม่สามารถโหลดไฟล์และรันในเครื่องเะื่อนได้เพื่อนจึงทำและส่งมารวมกัน
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
